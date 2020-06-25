2ce20e216d74f0de53b2a5df6df2ec8d634e69f2
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-06-25 15:46:00 -0700

    add mutex

--- telemetry/telemetry.go
+++ telemetry/telemetry.go
@@ -4,10 +4,18 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"sync"
 
 	"git.vzbuilders.com/marshadrad/panoptes/config"
 	"google.golang.org/grpc"
 )
+
+var reg = &telemetryRegister{nmi: make(map[string]NMIFactory)}
+
+type telemetryRegister struct {
+	nmi map[string]NMIFactory
+	sync.RWMutex
+}
 
 // NMIFactory ...
 type NMIFactory func(*grpc.ClientConn, []*config.Sensor, DSChan) NMI
@@ -23,12 +31,25 @@ type DataStore map[string]interface{}
 // DSChan ...
 type DSChan chan DataStore
 
-// R ...
-var R = make(map[string]NMIFactory)
+func (tr *telemetryRegister) set(name string, nf NMIFactory) {
+	tr.Lock()
+	defer tr.Unlock()
+	tr.nmi[name] = nf
+}
+
+func (tr *telemetryRegister) get(name string) NMIFactory {
+	tr.RLock()
+	defer tr.RUnlock()
+	return tr.nmi[name]
+}
 
 // Register ...
-func Register(n string, c NMIFactory) {
-	R[n] = c
+func Register(n string, nf NMIFactory) {
+	reg.set(n, nf)
+}
+
+func GetNMIFactory(name string) NMIFactory {
+	return reg.get(name)
 }
 
 func (ds DataStore) PrettyPrint() error {
