b8fbca0cb9236bdd2bcf1df518e847a010c62409
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-06-29 00:13:33 -0700

    init kafka segmentio

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/golang/protobuf v1.4.2
 	github.com/openconfig/gnmi v0.0.0-20200617225440-d2b4e6a45802
 	github.com/openconfig/ygot v0.8.0
+	github.com/segmentio/kafka-go v0.3.7
 	go.uber.org/zap v1.15.0
 	golang.org/x/net v0.0.0-20200625001655-4c5254603344
 	google.golang.org/grpc v1.30.0
--- producer/mqueue/kafka/kafka.go
+++ producer/mqueue/kafka/kafka.go
@@ -1,6 +1,12 @@
 package kafka
 
 import (
+	"context"
+	"encoding/json"
+	"strings"
+
+	kafka "github.com/segmentio/kafka-go"
+
 	"git.vzbuilders.com/marshadrad/panoptes/config"
 	"git.vzbuilders.com/marshadrad/panoptes/producer"
 	"git.vzbuilders.com/marshadrad/panoptes/telemetry"
@@ -8,20 +14,58 @@ import (
 )
 
 type Kafka struct {
-	ch telemetry.ExtDSChan
+	cfg config.Producer
+	ch  telemetry.ExtDSChan
+	lg  *zap.Logger
 }
 
 func New(cfg config.Producer, lg *zap.Logger, inChan telemetry.ExtDSChan) producer.Producer {
-	return &Kafka{ch: inChan}
+	return &Kafka{cfg: cfg, lg: lg, ch: inChan}
 }
 
 func (k *Kafka) Start() {
+	m := make(map[string]*kafka.Writer)
+	brokers := convArray(k.cfg.Config["brokers"])
+	topics := convArray(k.cfg.Config["topics"])
+
+	for _, topic := range topics {
+		m[topic] = kafka.NewWriter(kafka.WriterConfig{
+			Brokers:  brokers,
+			Topic:    topic,
+			Balancer: &kafka.LeastBytes{},
+		})
+	}
+
 	for {
 		v, ok := <-k.ch
 		if !ok {
 			break
 		}
 
-		v.DS.PrettyPrint("stdout")
+		out := strings.Split(v.Output, "::")
+		if len(out) < 2 {
+			k.lg.Error("wrong output", zap.String("output", v.Output))
+			continue
+		}
+		msg, err := json.Marshal(v.DS)
+		if err != nil {
+			k.lg.Error("marshal ds", zap.Error(err))
+			continue
+		}
+
+		err = m[out[1]].WriteMessages(context.Background(), kafka.Message{Value: msg})
+		if err != nil {
+			k.lg.Error("write message", zap.Error(err))
+		}
 	}
+}
+
+func convArray(i interface{}) []string {
+	var strArray []string
+	ifArray := i.([]interface{})
+	for _, v := range ifArray {
+		strArray = append(strArray, v.(string))
+	}
+
+	return strArray
 }
