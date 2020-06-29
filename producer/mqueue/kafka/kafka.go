512bfb80f2320c9cbf9051116df31e883a5cbc6f
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-06-29 12:30:41 -0700

    add batch & optimize multi topic

--- producer/mqueue/kafka/kafka.go
+++ producer/mqueue/kafka/kafka.go
@@ -4,68 +4,108 @@ import (
 	"context"
 	"encoding/json"
 	"strings"
+	"time"
 
-	kafka "github.com/segmentio/kafka-go"
+	"github.com/segmentio/kafka-go"
+	"go.uber.org/zap"
 
 	"git.vzbuilders.com/marshadrad/panoptes/config"
 	"git.vzbuilders.com/marshadrad/panoptes/producer"
 	"git.vzbuilders.com/marshadrad/panoptes/telemetry"
-	"go.uber.org/zap"
 )
 
+type kafkaConfig struct {
+	Brokers []string
+	Topics  []string
+
+	BatchSize int
+}
+
 type Kafka struct {
-	cfg config.Producer
+	cfg kafkaConfig
 	ch  telemetry.ExtDSChan
 	lg  *zap.Logger
 }
 
 func New(cfg config.Producer, lg *zap.Logger, inChan telemetry.ExtDSChan) producer.Producer {
-	return &Kafka{cfg: cfg, lg: lg, ch: inChan}
+	kConfig := convConfig(cfg.Config)
+	return &Kafka{cfg: kConfig, lg: lg, ch: inChan}
 }
 
 func (k *Kafka) Start() {
-	m := make(map[string]*kafka.Writer)
-	brokers := convArray(k.cfg.Config["brokers"])
-	topics := convArray(k.cfg.Config["topics"])
+	chMap := make(map[string]chan telemetry.DataStore)
 
-	for _, topic := range topics {
-		m[topic] = kafka.NewWriter(kafka.WriterConfig{
-			Brokers:  brokers,
-			Topic:    topic,
-			Balancer: &kafka.LeastBytes{},
-		})
+	for _, topic := range k.cfg.Topics {
+		chMap[topic] = make(chan telemetry.DataStore, 1)
+		go func(topic string) {
+			k.start(chMap[topic], topic)
+		}(topic)
 	}
 
 	for {
-		v, ok := <-k.ch
-		if !ok {
-			break
+		select {
+		case v, ok := <-k.ch:
+			if !ok {
+				break
+			}
+
+			topic := strings.Split(v.Output, "::")
+			if len(topic) < 2 {
+				k.lg.Error("topic not found", zap.String("output", v.Output))
+				continue
+			}
+
+			chMap[topic[1]] <- v.DS
+
+		}
+	}
+
+}
+
+func (k *Kafka) start(ch chan telemetry.DataStore, topic string) {
+	batch := make([]kafka.Message, 0, k.cfg.BatchSize)
+	flushTicker := time.NewTicker(time.Second * 10)
+	flush := false
+
+	w := kafka.NewWriter(kafka.WriterConfig{
+		Brokers:  k.cfg.Brokers,
+		Topic:    topic,
+		Balancer: &kafka.LeastBytes{},
+	})
+
+	for {
+		select {
+		case v, ok := <-ch:
+			if !ok {
+				break
+			}
+
+			b, err := json.Marshal(v)
+			if err != nil {
+				k.lg.Error("dataset marshal failed", zap.Error(err))
+			}
+
+			batch = append(batch, kafka.Message{Value: b})
+		case <-flushTicker.C:
+			flush = true
+
 		}
 
-		out := strings.Split(v.Output, "::")
-		if len(out) < 2 {
-			k.lg.Error("wrong output", zap.String("output", v.Output))
-			continue
-		}
-		msg, err := json.Marshal(v.DS)
-		if err != nil {
-			k.lg.Error("marshal ds", zap.Error(err))
-			continue
-		}
+		if len(batch) == k.cfg.BatchSize || flush {
+			err := w.WriteMessages(context.Background(), batch...)
+			if err != nil {
+				k.lg.Error("kafka write message failed", zap.Error(err))
+			}
 
-		err = m[out[1]].WriteMessages(context.Background(), kafka.Message{Value: msg})
-		if err != nil {
-			k.lg.Error("write message", zap.Error(err))
+			flush = false
+			batch = nil
 		}
 	}
 }
 
-func convArray(i interface{}) []string {
-	var strArray []string
-	ifArray := i.([]interface{})
-	for _, v := range ifArray {
-		strArray = append(strArray, v.(string))
-	}
-
-	return strArray
+func convConfig(c interface{}) kafkaConfig {
+	kc := kafkaConfig{}
+	b, _ := json.Marshal(c)
+	json.Unmarshal(b, &kc)
+	return kc
 }
