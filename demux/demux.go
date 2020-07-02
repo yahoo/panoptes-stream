8e3fcd0190dd6e5e29e45ef2c00f7fd5e7ae529d
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-07-02 10:30:32 -0700

    add subscribe & unsubscribe to demux

--- demux/demux.go
+++ demux/demux.go
@@ -12,44 +12,39 @@ import (
 )
 
 type Demux struct {
-	ctx    context.Context
-	cfg    config.Config
-	lg     *zap.Logger
-	inChan telemetry.ExtDSChan
-	chMap  map[string]telemetry.ExtDSChan
-	pr     *producer.Registrar
+	ctx      context.Context
+	cfg      config.Config
+	lg       *zap.Logger
+	inChan   telemetry.ExtDSChan
+	chMap    map[string]telemetry.ExtDSChan
+	pr       *producer.Registrar
+	register map[string]context.CancelFunc
 }
 
 func New(ctx context.Context, cfg config.Config, lg *zap.Logger, pr *producer.Registrar, inChan telemetry.ExtDSChan) *Demux {
 	return &Demux{
-		ctx:    ctx,
-		cfg:    cfg,
-		lg:     lg,
-		pr:     pr,
-		inChan: inChan,
-		chMap:  make(map[string]telemetry.ExtDSChan),
+		ctx:      ctx,
+		cfg:      cfg,
+		lg:       lg,
+		pr:       pr,
+		inChan:   inChan,
+		chMap:    make(map[string]telemetry.ExtDSChan),
+		register: make(map[string]context.CancelFunc),
 	}
 }
 
 func (d *Demux) Init() error {
 	for _, producer := range d.cfg.Producers() {
-		mqNew, ok := d.pr.GetProducerFactory(producer.Service)
-		if !ok {
-			return errors.New("producer not exist")
+		err := d.subscribe(producer)
+		if err != nil {
+			return err
 		}
-
-		// register channel
-		d.chMap[producer.Name] = make(telemetry.ExtDSChan, 1)
-		// construct
-		m := mqNew(d.ctx, producer, d.lg, d.chMap[producer.Name])
-		// start the producer
-		go m.Start()
 	}
 
 	return nil
 }
 
-func (d *Demux) Start(ctx context.Context) {
+func (d *Demux) Start() {
 	for {
 		extDS, _ := <-d.inChan
 		output := strings.Split(extDS.Output, "::")
@@ -60,4 +55,29 @@ func (d *Demux) Start(ctx context.Context) {
 		d.chMap[output[0]] <- extDS
 
 	}
+}
+
+func (d *Demux) subscribe(producer config.Producer) error {
+	var ctx context.Context
+
+	mqNew, ok := d.pr.GetProducerFactory(producer.Service)
+	if !ok {
+		return errors.New("producer not exist")
+	}
+
+	// register channel
+	d.chMap[producer.Name] = make(telemetry.ExtDSChan, 1)
+	// register cancelFunnc
+	ctx, d.register[producer.Name] = context.WithCancel(d.ctx)
+	// construct
+	m := mqNew(ctx, producer, d.lg, d.chMap[producer.Name])
+	// start the producer
+	go m.Start()
+
+	return nil
+}
+
+func (d *Demux) unsubscribe(producer config.Producer) {
+	cancel := d.register[producer.Name]
+	cancel()
 }
