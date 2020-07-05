8f6365e63750a1ca807764b80cbd745e8718182e
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-07-05 15:02:59 -0700

    add cli

--- panoptes/option.go
+++ panoptes/option.go
@@ -1,1 +1,75 @@
-package main 
+package main
+
+import (
+	"errors"
+	"os"
+
+	"git.vzbuilders.com/marshadrad/panoptes/config"
+	"git.vzbuilders.com/marshadrad/panoptes/config/consul"
+	"git.vzbuilders.com/marshadrad/panoptes/config/yaml"
+	cli "github.com/urfave/cli/v2"
+)
+
+type cmd struct {
+	configFile string
+	consul     bool
+}
+
+func getConfig() (config.Config, error) {
+	var cfg config.Config
+
+	cli, err := getCli()
+	if err != nil {
+		return nil, err
+	}
+
+	if cli.consul {
+		cfg, err = consul.New("etc/consul.yaml")
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		cfg, err = yaml.New("etc/config.yaml")
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return cfg, nil
+}
+
+func getCli() (*cmd, error) {
+	cm := cmd{}
+
+	flags := []cli.Flag{
+		&cli.StringFlag{
+			Name:  "config",
+			Usage: "path to a file in yaml format to read configuration",
+		},
+		&cli.BoolFlag{
+			Name:  "consul",
+			Usage: "enable consul configuration management",
+		},
+	}
+
+	app := &cli.App{
+		Flags: flags,
+		Action: func(c *cli.Context) error {
+			cm = cmd{
+				configFile: c.String("config"),
+				consul:     c.Bool("consul"),
+			}
+
+			if c.NumFlags() < 1 {
+				cli.ShowAppHelp(c)
+				return errors.New("configuration not specified")
+			}
+
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+
+	return &cm, err
+}
--- panoptes/panoptes.go
+++ panoptes/panoptes.go
@@ -2,32 +2,41 @@ package main
 
 import (
 	"context"
+	"time"
 
-	"git.vzbuilders.com/marshadrad/panoptes/config/yaml"
+	"git.vzbuilders.com/marshadrad/panoptes/config"
+	"git.vzbuilders.com/marshadrad/panoptes/database"
 	"git.vzbuilders.com/marshadrad/panoptes/demux"
+	"git.vzbuilders.com/marshadrad/panoptes/discovery/consul"
 	"git.vzbuilders.com/marshadrad/panoptes/producer"
 	"git.vzbuilders.com/marshadrad/panoptes/register"
+	"git.vzbuilders.com/marshadrad/panoptes/status"
 	"git.vzbuilders.com/marshadrad/panoptes/telemetry"
 )
 
 var (
 	producerRegistrar  *producer.Registrar
+	databaseRegistrar  *database.Registrar
 	telemetryRegistrar *telemetry.Registrar
 )
 
 func main() {
-	// cfg, err := consul.New("etc/consul.yaml")
-	// if err != nil {
-	// 	log.Println(err)
-	// }
+	cfg, err := getConfig()
+	if err != nil {
+		panic(err)
+	}
 
-	//_ = cs
-
-	cfg, _ := yaml.New("etc/config.yaml")
-	lg := GetLogger(cfg.Global().Logger)
+	lg := cfg.Logger()
 	defer lg.Sync()
 
 	lg.Info("starting ...")
+
+	discovery, err := consul.New(cfg)
+	if err != nil {
+		panic(err)
+	}
+	discovery.Register()
+	defer discovery.Deregister()
 
 	ctx := context.Background()
 
@@ -35,19 +44,48 @@ func main() {
 	producerRegistrar = producer.NewRegistrar(lg)
 	register.Producer(producerRegistrar)
 
+	// database
+	databaseRegistrar = database.NewRegistrar(lg)
+	register.Database(databaseRegistrar)
+
 	// telemetry
 	telemetryRegistrar = telemetry.NewRegistrar(lg)
 	register.Telemetry(telemetryRegistrar)
 
 	outChan := make(telemetry.ExtDSChan, 1)
 
-	dp := demux.New(cfg, lg, producerRegistrar, outChan)
+	dp := demux.New(ctx, cfg, producerRegistrar, databaseRegistrar, outChan)
 	dp.Init()
-	go dp.Start(ctx)
+	go dp.Start()
 
-	p := telemetry.New(ctx, cfg, lg, telemetryRegistrar, outChan)
-	p.Watcher()
-	p.Start()
+	t := telemetry.New(ctx, cfg, telemetryRegistrar, outChan)
+	t.Start()
 
-	<-ctx.Done()
+	s := status.New(cfg)
+	s.Start()
+
+	ticker := time.NewTicker(time.Second * 60)
+	for {
+		<-ticker.C
+		devices := make(map[string]config.Device)
+		producers := make(map[string]config.Producer)
+		databases := make(map[string]config.Database)
+
+		for _, device := range cfg.Devices() {
+			devices[device.Host] = device
+		}
+
+		for _, producer := range cfg.Producers() {
+			producers[producer.Name] = producer
+		}
+
+		for _, database := range cfg.Databases() {
+			databases[database.Name] = database
+		}
+
+		cfg.Update()
+		dp.Update(producers, databases)
+		t.Update(devices)
+	}
+
 }
