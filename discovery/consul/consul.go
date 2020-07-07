0431f5df8d749eaee83af99186993b859bb17d7f
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-07-07 09:10:37 -0700

    init GetInstances

--- discovery/consul/consul.go
+++ discovery/consul/consul.go
@@ -32,9 +32,11 @@ func New(cfg config.Config) (discovery.Discovery, error) {
 
 // Register registers the panoptes at consul
 func (c *Consul) Register() {
+	meta := make(map[string]string)
 	reg := &api.AgentServiceRegistration{
 		ID:      "panoptes",
 		Name:    "panoptes",
+		Meta:    meta,
 		Address: hostname(),
 	}
 
@@ -47,6 +49,25 @@ func (c *Consul) Register() {
 	if err := c.client.Agent().ServiceRegister(reg); err != nil {
 		c.logger.Error("register failed", zap.Error(err))
 	}
+}
+
+// GetInstances returns all registered instances
+func (c *Consul) GetInstances() []discovery.Instance {
+	var instances []discovery.Instance
+	_, checksInfo, err := c.client.Agent().AgentHealthServiceByName("panoptes")
+	if err != nil {
+		c.logger.Error("get health service failed", zap.Error(err))
+	}
+
+	for _, info := range checksInfo {
+		instances = append(instances, discovery.Instance{
+			ID:      info.Service.ID,
+			Meta:    info.Service.Meta,
+			Address: info.Service.Address,
+			Status:  info.Checks.AggregatedStatus(),
+		})
+	}
+	return []discovery.Instance{}
 }
 
 // Deregister deregisters the panoptes at consul
--- discovery/discovery.go
+++ discovery/discovery.go
@@ -3,4 +3,12 @@ package discovery
 type Discovery interface {
 	Register()
 	Deregister()
+	GetInstances() []Instance
+}
+
+type Instance struct {
+	ID      string
+	Meta    map[string]string
+	Address string
+	Status  string
 }
