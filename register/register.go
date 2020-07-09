19dbb866ae4c618fa1b6cc12665afcd51397aff2
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-07-09 15:59:07 -0700

    register arista

--- register/register.go
+++ register/register.go
@@ -7,11 +7,13 @@ import (
 	"git.vzbuilders.com/marshadrad/panoptes/producer/console"
 	"git.vzbuilders.com/marshadrad/panoptes/producer/mqueue"
 	"git.vzbuilders.com/marshadrad/panoptes/telemetry"
+	"git.vzbuilders.com/marshadrad/panoptes/telemetry/arista"
 	"git.vzbuilders.com/marshadrad/panoptes/telemetry/juniper"
 )
 
 func Telemetry(telemetryRegistrar *telemetry.Registrar) {
 	juniper.Register(telemetryRegistrar)
+	arista.Register(telemetryRegistrar)
 }
 
 func Producer(producerRegistrar *producer.Registrar) {
