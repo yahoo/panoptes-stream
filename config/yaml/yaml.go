5378f1c6e92110a5ad3707c1dc8558823741e169
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-07-13 10:53:26 -0700

    merge watcher

--- config/yaml/yaml.go
+++ config/yaml/yaml.go
@@ -4,10 +4,11 @@ import (
 	"io/ioutil"
 	"log"
 
-	"git.vzbuilders.com/marshadrad/panoptes/config"
+	"github.com/fsnotify/fsnotify"
 	"go.uber.org/zap"
-
 	yml "gopkg.in/yaml.v2"
+
+	"git.vzbuilders.com/marshadrad/panoptes/config"
 )
 
 type yaml struct {
@@ -183,4 +184,51 @@ func configDatabases(p map[string]database) []config.Database {
 	}
 
 	return databases
+}
+
+func (y *yaml) watcher() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan struct{})
+	go func() {
+		defer close(done)
+
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					select {
+					case y.informer <- struct{}{}:
+					default:
+					}
+
+					y.logger.Info("watcher", zap.String("name", event.Name))
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add(y.filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	<-done
+}
+
+func (y *yaml) Informer() chan struct{} {
+	return y.informer
 }
