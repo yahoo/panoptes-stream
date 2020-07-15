fa3f651da6176052eb6c3e04328f5289a6015015
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-07-15 09:18:44 -0700

    init data ingest - line protocol

--- database/tsdb/influxdb/influxdb.go
+++ database/tsdb/influxdb/influxdb.go
@@ -2,12 +2,16 @@ package influxdb
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"strings"
 
 	"git.vzbuilders.com/marshadrad/panoptes/config"
 	"git.vzbuilders.com/marshadrad/panoptes/database"
 	"git.vzbuilders.com/marshadrad/panoptes/telemetry"
 	"go.uber.org/zap"
+
+	influxdb2 "github.com/influxdata/influxdb-client-go"
 )
 
 type InfluxDB struct {
@@ -15,6 +19,12 @@ type InfluxDB struct {
 	ch  telemetry.ExtDSChan
 	lg  *zap.Logger
 	cfg config.Database
+}
+
+type influxDBConfig struct {
+	Server   string
+	Database string
+	Token    string
 }
 
 func New(ctx context.Context, cfg config.Database, lg *zap.Logger, inChan telemetry.ExtDSChan) database.Database {
@@ -27,9 +37,20 @@ func New(ctx context.Context, cfg config.Database, lg *zap.Logger, inChan teleme
 }
 
 func (i *InfluxDB) Start() {
+	var (
+		tagSet, fieldSet       []string
+		measurement, timestamp string
+		config                 = new(influxDBConfig)
+	)
 
 	i.lg.Info("influxdb set up", zap.String("name", i.cfg.Name),
 		zap.String("server url", i.cfg.Config["server"].(string)))
+
+	b, _ := json.Marshal(i.cfg.Config)
+	json.Unmarshal(b, config)
+
+	client := influxdb2.NewClient(config.Server, config.Token)
+	writeApi := client.WriteApi("", config.Database)
 
 	for {
 		select {
@@ -44,11 +65,50 @@ func (i *InfluxDB) Start() {
 				continue
 			}
 
-			v.DS.PrettyPrint(out[1])
+			measurement = out[1]
+
+			for k, v := range v.DS {
+				switch k {
+				case "__prefix__":
+					tagSet = append(tagSet, fmt.Sprintf("path=%s", v.(string)))
+				case "__labels__":
+					labels := v.(map[string]string)
+					for k, v := range labels {
+						tagSet = append(tagSet, fmt.Sprintf("%s=%s", k, v))
+					}
+				case "__system_id__":
+					tagSet = append(tagSet, fmt.Sprintf("system_id=%s", v.(string)))
+				case "__timestamp__":
+					timestamp = getValueString(v)
+				default:
+					fieldSet = append(fieldSet, fmt.Sprintf(" %s=%s", k, getValueString(v)))
+				}
+			}
+
+			line := fmt.Sprintf("%s,%s %s %s", measurement, strings.Join(tagSet, ","), strings.Join(fieldSet, ","), timestamp)
+			writeApi.WriteRecord(line)
+
+			tagSet = tagSet[:0]
+			fieldSet = fieldSet[:0]
+
 		case <-i.ctx.Done():
 			i.lg.Info("database has been terminated", zap.String("name", i.cfg.Name))
 			return
 		}
 	}
 
+}
+
+func getValueString(value interface{}) string {
+	switch v := value.(type) {
+	case uint64, uint32, uint16, uint8, uint,
+		int64, int32, int16, int8, int:
+		return fmt.Sprintf("%d", v)
+	case float64, float32:
+		return fmt.Sprintf("%f", v)
+	case string:
+		return v
+	}
+
+	return ""
 }
