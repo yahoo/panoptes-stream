5c4e1f7a8c2840433324aa03891cf0e0fc80208b
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-07-22 08:43:44 -0700

    add gettlsconfig to secret (local and remote)

--- secret/secret.go
+++ secret/secret.go
@@ -1,16 +1,19 @@
 package secret
 
 import (
-	"context"
 	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"io/ioutil"
+	"regexp"
 
+	"git.vzbuilders.com/marshadrad/panoptes/config"
 	"git.vzbuilders.com/marshadrad/panoptes/secret/vault"
 )
 
 type Secret interface {
-	GetCredentials(context.Context, string) ([]string, error)
-	GetCertificate(context.Context, string) (*tls.Certificate, error)
+	GetCredentials(string) ([]string, error)
+	GetCertificate(string) (*tls.Certificate, error)
 }
 
 func GetSecretEngine(sType string) (Secret, error) {
@@ -20,4 +23,70 @@ func GetSecretEngine(sType string) (Secret, error) {
 	}
 
 	return nil, fmt.Errorf("%s secret engine doesn't support", sType)
+}
+
+func GetTLSConfig(cfg *config.TLSConfig) (*tls.Config, error) {
+	tlsConfig, ok, err := getTLSConfigRemote(cfg)
+	if ok {
+		return tlsConfig, err
+	}
+
+	return getTLSConfigLocal(cfg)
+}
+
+func ParseRemoteSecretInfo(key string) (string, string, bool) {
+	re := regexp.MustCompile(`__([a-zA-Z0-9]*)::(.*)`)
+	match := re.FindStringSubmatch(key)
+	if len(match) < 1 {
+		return "", "", false
+	}
+
+	return match[1], match[2], true
+}
+
+func getTLSConfigRemote(cfg *config.TLSConfig) (*tls.Config, bool, error) {
+	sType, path, ok := ParseRemoteSecretInfo(cfg.CertFile)
+	if ok {
+		sec, err := GetSecretEngine(sType)
+		if err != nil {
+			return nil, ok, err
+		}
+
+		cert, err := sec.GetCertificate(path)
+		if err != nil {
+			return nil, ok, err
+		}
+
+		return &tls.Config{
+			Certificates:       []tls.Certificate{*cert},
+			InsecureSkipVerify: cfg.InsecureSkipVerify,
+		}, ok, nil
+	}
+
+	return nil, false, nil
+}
+
+func getTLSConfigLocal(cfg *config.TLSConfig) (*tls.Config, error) {
+	var caCertPool *x509.CertPool
+
+	cert, err := tls.LoadX509KeyPair(cfg.CertFile, cfg.KeyFile)
+	if err != nil {
+		return nil, err
+	}
+
+	if cfg.CAFile != "" {
+		caCert, err := ioutil.ReadFile(cfg.CAFile)
+		if err != nil {
+			return nil, err
+		}
+
+		caCertPool = x509.NewCertPool()
+		caCertPool.AppendCertsFromPEM(caCert)
+	}
+
+	return &tls.Config{
+		Certificates:       []tls.Certificate{cert},
+		RootCAs:            caCertPool,
+		InsecureSkipVerify: cfg.InsecureSkipVerify,
+	}, nil
 }
