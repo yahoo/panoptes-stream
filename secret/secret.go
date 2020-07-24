c5f3b8db11b0bf553b28521edae137439de1d065
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-07-23 17:39:18 -0700

    add new method: GetKeyPair

--- secret/secret.go
+++ secret/secret.go
@@ -14,6 +14,7 @@ import (
 type Secret interface {
 	GetCredentials(string) ([]string, error)
 	GetCertificate(string) (*tls.Certificate, error)
+	GetKeyPair(string) ([]byte, []byte, error)
 }
 
 func GetSecretEngine(sType string) (Secret, error) {
--- secret/vault/vault.go
+++ secret/vault/vault.go
@@ -7,7 +7,6 @@ import (
 	"encoding/base64"
 	"encoding/pem"
 	"errors"
-	"reflect"
 
 	"github.com/hashicorp/vault/api"
 	"software.sslmate.com/src/go-pkcs12"
@@ -45,41 +44,50 @@ func (v *Vault) GetCredentials(path string) ([]string, error) {
 
 // GetCertificate returns TLS certificate from Vault
 func (v *Vault) GetCertificate(path string) (*tls.Certificate, error) {
+	cert, key, err := v.GetKeyPair(path)
+	if err != nil {
+		return nil, err
+	}
+
+	certificate, err := tls.X509KeyPair(cert, key)
+	if err != nil {
+		return nil, err
+	}
+
+	return &certificate, nil
+}
+
+// GetKeyPair returns x509 key pair from Vault
+func (v *Vault) GetKeyPair(path string) ([]byte, []byte, error) {
 	cfg := api.DefaultConfig()
 	client, err := api.NewClient(cfg)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	secrets, err := client.Logical().Read(path)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	keys := reflect.ValueOf(secrets.Data).MapKeys()
-	if len(keys) < 1 {
-
+	if isExist(secrets.Data, "cert") {
+		return getKeyPair(secrets.Data)
+	} else if isExist(secrets.Data, "pkcs12") {
+		return getKeyPairPKCS12PEM(secrets.Data)
 	}
 
-	switch {
-	case isExist(secrets.Data, "pkcs12"):
-		return pkcs12pem(secrets.Data)
-	case isExist(secrets.Data, "cert"):
-		return certX509(secrets.Data)
-	}
-
-	return nil, errors.New("not exist")
+	return nil, nil, errors.New("not exist")
 }
 
-// pkcs12pem returns certificate from pkcs12 archive
+// getKeyPairPKCS12PEM returns certificate from pkcs12 archive
 // private key and X.509 certificate encoded as PEM
 // pkcs12=pkcs12_data password=password
 // password is optional
-func pkcs12pem(data map[string]interface{}) (*tls.Certificate, error) {
+func getKeyPairPKCS12PEM(data map[string]interface{}) ([]byte, []byte, error) {
 	password := ""
 	b, err := base64.StdEncoding.DecodeString(data["pkcs12"].(string))
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	if isExist(data, "password") {
@@ -88,36 +96,31 @@ func pkcs12pem(data map[string]interface{}) (*tls.Certificate, error) {
 
 	key, cert, err := pkcs12.Decode(b, password)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	privateKey, err := x509.MarshalPKCS8PrivateKey(key)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	keyPEM := &bytes.Buffer{}
 	err = pem.Encode(keyPEM, &pem.Block{Type: "PRIVATE KEY", Bytes: privateKey})
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	certPEM := &bytes.Buffer{}
 	err = pem.Encode(certPEM, &pem.Block{Type: "CERTIFICATE", Bytes: cert.Raw})
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	certificate, err := tls.X509KeyPair(certPEM.Bytes(), keyPEM.Bytes())
-	if err != nil {
-		return nil, err
-	}
-
-	return &certificate, nil
+	return certPEM.Bytes(), keyPEM.Bytes(), nil
 }
 
-// private key and certificate encoded as PEM
-func certX509(data map[string]interface{}) (*tls.Certificate, error) {
+// getKeyPair returns private key and certificate encoded as PEM
+func getKeyPair(data map[string]interface{}) ([]byte, []byte, error) {
 	var key string
 
 	cert := data["cert"].(string)
@@ -128,12 +131,7 @@ func certX509(data map[string]interface{}) (*tls.Certificate, error) {
 		key = cert
 	}
 
-	certificate, err := tls.X509KeyPair([]byte(cert), []byte(key))
-	if err != nil {
-		return nil, err
-	}
-
-	return &certificate, nil
+	return []byte(cert), []byte(key), nil
 }
 
 func isExist(m map[string]interface{}, k string) bool {
