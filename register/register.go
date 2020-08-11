a8a06ecb00a282a6fadf395b5222b304191f8e17
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-08-11 00:20:59 -0700

    rename generic to arista

--- register/register.go
+++ register/register.go
@@ -7,15 +7,15 @@ import (
 	"git.vzbuilders.com/marshadrad/panoptes/producer/console"
 	"git.vzbuilders.com/marshadrad/panoptes/producer/mqueue"
 	"git.vzbuilders.com/marshadrad/panoptes/telemetry"
+	"git.vzbuilders.com/marshadrad/panoptes/telemetry/arista"
 	"git.vzbuilders.com/marshadrad/panoptes/telemetry/cisco"
-	"git.vzbuilders.com/marshadrad/panoptes/telemetry/generic"
 	"git.vzbuilders.com/marshadrad/panoptes/telemetry/juniper"
 )
 
 func Telemetry(telemetryRegistrar *telemetry.Registrar) {
 	juniper.Register(telemetryRegistrar)
 	cisco.Register(telemetryRegistrar)
-	generic.Register(telemetryRegistrar)
+	arista.Register(telemetryRegistrar)
 }
 
 func Producer(producerRegistrar *producer.Registrar) {
--- telemetry/arista/arista.go
+++ telemetry/arista/arista.go
@@ -1,10 +1,10 @@
-package generic
+package arista
 
 import (
 	"git.vzbuilders.com/marshadrad/panoptes/telemetry"
-	"git.vzbuilders.com/marshadrad/panoptes/telemetry/generic/gnmi"
+	"git.vzbuilders.com/marshadrad/panoptes/telemetry/arista/gnmi"
 )
 
 func Register(telemetryRegistrar *telemetry.Registrar) {
-	telemetryRegistrar.Register("generic.gnmi", gnmi.Version(), gnmi.New)
+	telemetryRegistrar.Register("arista.gnmi", gnmi.Version(), gnmi.New)
 }
