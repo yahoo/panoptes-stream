1177cb23485bf82fdd738aead2200fd9428c8b33
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-08-12 12:50:52 -0700

    register cisco mdt

--- telemetry/cisco/cisco.go
+++ telemetry/cisco/cisco.go
@@ -3,8 +3,10 @@ package cisco
 import (
 	"git.vzbuilders.com/marshadrad/panoptes/telemetry"
 	"git.vzbuilders.com/marshadrad/panoptes/telemetry/cisco/gnmi"
+	"git.vzbuilders.com/marshadrad/panoptes/telemetry/cisco/mdt"
 )
 
 func Register(telemetryRegistrar *telemetry.Registrar) {
 	telemetryRegistrar.Register("cisco.gnmi", gnmi.Version(), gnmi.New)
+	telemetryRegistrar.Register("cisco.mdt", mdt.Version(), mdt.New)
 }
