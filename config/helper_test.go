eebd5470973672a1d430fde2ffc45a8cba2792df
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-09-14 10:14:21 -0700

    add more tests

--- config/helper_test.go
+++ config/helper_test.go
@@ -50,4 +50,38 @@ func TestGetLogger(t *testing.T) {
 	lcfg := make(map[string]interface{})
 	json.Unmarshal(rawJSON, &lcfg)
 	assert.NotNil(t, GetLogger(lcfg))
+	assert.NotNil(t, GetLogger(nil))
+}
+
+func TestSensorValidation(t *testing.T) {
+	sensor := Sensor{Service: "juniper.gnmi"}
+	err := SensorValidation(sensor)
+	assert.NoError(t, err)
+	sensor = Sensor{Service: "noname.gnmi"}
+	err = SensorValidation(sensor)
+	assert.Error(t, err)
+}
+
+func TestSetDefaultGlobal(t *testing.T) {
+	g := Global{}
+	SetDefaultGlobal(&g)
+	assert.Greater(t, g.BufferSize, 0)
+	assert.Greater(t, g.OutputBufferSize, 0)
+	assert.NotEmpty(t, g.Version)
+}
+
+func TestSetDefault(t *testing.T) {
+	v := 0
+	SetDefault(&v, 5)
+	assert.Equal(t, 5, v)
+
+	SetDefault(&v, 6)
+	assert.Equal(t, 5, v)
+
+	vv := uint(0)
+	SetDefault(&vv, 5)
+	assert.Equal(t, uint(5), vv)
+
+	SetDefault(&vv, 6)
+	assert.Equal(t, uint(5), vv)
 }
