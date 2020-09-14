3bd098c733c37c40dc7efd32cd550f3b51302627
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-09-14 09:56:04 -0700

    add more test

--- database/tsdb/influxdb/influxdb_test.go
+++ database/tsdb/influxdb/influxdb_test.go
@@ -5,11 +5,17 @@ package influxdb
 
 import (
 	"bytes"
+	"context"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"git.vzbuilders.com/marshadrad/panoptes/config"
 	"git.vzbuilders.com/marshadrad/panoptes/telemetry"
 )
 
@@ -31,6 +37,50 @@ func TestLineProtocol(t *testing.T) {
 	l, err := getLineProtocol(buf, data)
 	require.Equal(t, err, nil)
 	assert.Equal(t, l, "ifcounters,_prefix_=/interfaces/interface/state/counters/,_host_=core1.bur,name=Ethernet3 out-octets=5587651 1595768623436661269")
+}
+
+func TestSingleMetric(t *testing.T) {
+	done := make(chan struct{})
+	ctx, cancel := context.WithCancel(context.Background())
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, _ := ioutil.ReadAll(r.Body)
+		assert.Equal(t, "test,_prefix_=/tests/test,_host_=127.0.0.1 mykey=0 150000000\n", string(body))
+		w.WriteHeader(http.StatusAccepted)
+		close(done)
+	}))
+
+	cfg := config.NewMockConfig()
+	ch := make(telemetry.ExtDSChan, 10)
+
+	dbCfg := config.Database{Name: "influxdb1", Service: "influxdb", Config: map[string]interface{}{
+		"server":     server.URL,
+		"bucket":     "mybucket",
+		"bactchSize": 1,
+	}}
+
+	db := New(ctx, dbCfg, cfg.Logger(), ch)
+	go db.Start()
+	ch <- telemetry.ExtDataStore{
+		Output: "influxdb1::test",
+		DS: map[string]interface{}{
+			"prefix":    "/tests/test",
+			"labels":    map[string]string{},
+			"system_id": "127.0.0.1",
+			"timestamp": 150000000,
+			"key":       "mykey",
+			"value":     0,
+		},
+	}
+
+	select {
+	case <-done:
+	case <-time.After(3 * time.Second):
+		t.Error("time limit exceeded")
+	}
+
+	time.Sleep(time.Second)
+	cancel()
 }
 
 func BenchmarkLineProtocol(b *testing.B) {
