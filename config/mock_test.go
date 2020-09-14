14a766872a2305c114e58cc85a284e99e28adcb2
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-09-14 10:15:33 -0700

    add more test

--- config/mock_test.go
+++ config/mock_test.go
@@ -6,9 +6,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMock(t *testing.T) {
-	cfg := NewMockConfig()
+var cfg = NewMockConfig()
 
+func TestMock(t *testing.T) {
 	assert.Equal(t, "0.0.0", cfg.Global().Version)
 	cfg.Logger().Info("hello mock")
 	log := cfg.LogOutput.Unmarshal()
@@ -24,4 +24,25 @@ func TestMock(t *testing.T) {
 	assert.Equal(t, []Producer{{Name: "kafka"}}, cfg.Producers())
 	assert.Equal(t, []Sensor{{Service: "juniper.gnmi"}}, cfg.Sensors())
 	assert.Equal(t, []Device{{DeviceConfig: DeviceConfig{Host: "core1.lax"}}}, cfg.Devices())
+
+	err := cfg.Update()
+	ch := cfg.Informer()
+
+	assert.NoError(t, err)
+	assert.Nil(t, ch)
+}
+
+func TestUnmarshalSlice(t *testing.T) {
+	cfg.LogOutput.Reset()
+
+	cfg.logger.Info("test_info")
+	cfg.logger.Error("test_error")
+
+	s := cfg.LogOutput.UnmarshalSlice()
+	assert.Len(t, s, 2)
+	assert.Equal(t, "test_info", s[0]["M"])
+	assert.Equal(t, "test_error", s[1]["M"])
+
+	cfg.LogOutput.Sync()
+	cfg.LogOutput.Close()
 }
