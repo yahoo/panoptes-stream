a234c8f3ea54711b7e365b5880c65eb5efe8b050
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-09-16 09:07:10 -0700

    add test

---
+++ producer/console/console_test.go
@@ -0,0 +1,48 @@
+//: Copyright Verizon Media
+//: Licensed under the terms of the Apache 2.0 License. See LICENSE file in the project root for terms
+
+package console
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"git.vzbuilders.com/marshadrad/panoptes/config"
+	"git.vzbuilders.com/marshadrad/panoptes/telemetry"
+)
+
+func TestConsole(t *testing.T) {
+	stdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	cfg := config.NewMockConfig()
+	ch := make(telemetry.ExtDSChan, 2)
+	p := New(context.Background(), config.Producer{}, cfg.Logger(), ch)
+	go p.Start()
+
+	ch <- telemetry.ExtDataStore{
+		Output: "console::stdout",
+		DS:     telemetry.DataStore{"test": "test"},
+	}
+
+	buf := new(bytes.Buffer)
+	io.CopyN(buf, r, 20)
+	os.Stdout = stdout
+	assert.Contains(t, buf.String(), "test")
+
+	ch <- telemetry.ExtDataStore{
+		Output: "console",
+		DS:     telemetry.DataStore{"test": "test"},
+	}
+	time.Sleep(time.Second)
+	assert.Contains(t, cfg.LogOutput.String(), "wrong output")
+
+	close(ch)
+}
